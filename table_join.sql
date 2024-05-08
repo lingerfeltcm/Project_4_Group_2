4bb1997c3a0251cc38d4318adfc2eedc54e372a8
comfyben <bened.comfort@gmail.com>
2024-05-07 21:10:58 -0500

    join SQL query update

--- table_join.sql
+++ table_join.sql
@@ -1,11 +1,13 @@
 SELECT c."country", c."code", c."region", c."income", c."year", c."che_gdp(%)", c."hk_gdl(%)", c."che_pc_usd", c."che", c."gdp_pc_usd", c."che_usd",
-a."Sex", a."Age_group_code", a."Age_Group", a."Number", a."Age_Stnd_Per_100k", a."Death_rate_per_100k"
+a."Year", a."Number", a."Percent_of_Total", a."Age_Stnd_Per_100k", a."Death_rate_per_100k"
 
 INTO ExpMortJoin
 
 FROM expenditures c 
-INNER JOIN clean_infectious_disease a ON c."country" = a."Country_Name"
+INNER JOIN clean_infectious_disease a ON c."country" = a."Country_Name" AND c."year" = a."Year"
 ;
 
 SELECT * FROM ExpMortJoin;
+
+DROP TABLE ExpMortJoin;
 
